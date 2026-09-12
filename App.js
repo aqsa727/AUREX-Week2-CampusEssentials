1cdeffca6bc057b3e3d9d3a53e98ab1ce54fbaf5
aqsa727 <135244993+aqsa727@users.noreply.github.com>
2026-09-12 13:44:43 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+// App.js
+// Main entry point of the app. Just renders the root navigator.
+
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return <AppNavigator />;
+}
